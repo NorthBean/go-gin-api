184f55c65191514d7edabb8109e08b31e6da3840
shenguojun <shenguojun@qianxin.com>
2023-05-22 16:00:50 +0800

    对shutdown包增加注释

--- pkg/shutdown/shutdown.go
+++ pkg/shutdown/shutdown.go
@@ -6,6 +6,7 @@ import (
 	"syscall"
 )
 
+// 这里为了确保hook实现了Hook接口，在编译时检查这种实现是否正确，提高代码的可维护性和健壮性。
 var _ Hook = (*hook)(nil)
 
 // Hook a graceful shutdown hook, default with signals of SIGINT and SIGTERM
@@ -26,11 +27,12 @@ func NewHook() Hook {
 	hook := &hook{
 		ctx: make(chan os.Signal, 1),
 	}
-
+	// 监听信号
 	return hook.WithSignals(syscall.SIGINT, syscall.SIGTERM)
 }
 
 func (h *hook) WithSignals(signals ...syscall.Signal) Hook {
+	// 监听信号，如果有信号传入，就会往ctx中写入数据
 	for _, s := range signals {
 		signal.Notify(h.ctx, s)
 	}
@@ -39,9 +41,11 @@ func (h *hook) WithSignals(signals ...syscall.Signal) Hook {
 }
 
 func (h *hook) Close(funcs ...func()) {
+	// 从ctx中读取数据，如果没有数据，就会阻塞，读取到数据则说明有信号传入，就会执行funcs中的函数
 	select {
 	case <-h.ctx:
 	}
+	// 关闭监听
 	signal.Stop(h.ctx)
 
 	for _, f := range funcs {
